c50f7ef76e222d564aab6c3527e1bd6c22824682
Prodipta Nayeb <nayebprodipta@gmail.com>
2025-11-02 18:16:08 +0530

    Add files via upload

---
+++ frequency.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<string.h>
+void main()
+{
+    char find[100],sen[100],word[100]="",ch;
+    int i=0,j=0,h=0,k=0,m=0,end=0;
+    printf("Enter any sentence:\n");
+    fgets(sen,100,stdin);
+    printf("Enter the word you want to find:\n");
+    scanf("%s",find);//entering the word to be find.
+    int flag;
+    for(i=0;sen[i]!='\0';i++);//Calculating the length of the string.
+    for(m=0;find[m]!='\0';m++);//calculating the length of the word to be find.
+    end= i-m+1;
+    k=0;
+    for(j=0;j<end;j++)
+    {
+        flag=0;//true;
+        for(h=0;h<m;h++)
+        {
+            if(find[h]!=sen[j+h])
+            {
+                flag=1;//false
+                break;
+            }
+        }
+        if(flag==0)
+        {
+            k++;
+        }
+    }
+    printf("%d",k);
+    
+}
---
+++ rev_string.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<string.h>
+void rev(char *sen);
+void main()
+{
+     char s[100];
+    printf("Enter any sentence:\n");
+    fgets(s,100,stdin);
+
+    //this segment is to avoid printing of the last word in nwe line.
+    if (s[strlen(s) - 1] == '\n')
+    {
+        s[strlen(s) - 1] = '\0';
+    }
+   rev(s);
+   printf("%s",s);
+}
+void rev(char *sen)
+{
+    char  word[100];
+    int i=0,j=0,len=0;
+    len= strlen(sen);
+    for(i=0;i<len;i++)//running till the end of the string.
+    {
+        for(j=0;i<len;j++,i++)// Identify the word.
+        {
+            if(sen[i]==' '||sen[i]=='\0')//terminating the word when ' ' and '\0'.
+            {
+                break;
+            }
+            word[j]=sen[i];//storing the letters of the word one by one in a temporary array.
+        }
+        //we are going to access the word one index earlier if j is the length of the word.
+        while(j>0)
+        {
+            j--;
+            sen[i-j-1]= word[j];
+        }
+    }
+}
+
+
+
+
+/*#include <stdio.h>
+
+int main() {
+    char str[] = "hello world from copilot";
+    int i = 0, start = 0, end;
+
+    // Traverse the string
+    while (str[i] != '\0') {
+        // Find the start of a word
+        if (str[i] != ' ') {
+            start = i;
+
+            // Move to the end of the word
+            while (str[i] != ' ' && str[i] != '\0') {
+                i++;
+            }
+            end = i - 1;
+
+            // Reverse the word manually
+            while (start < end) {
+                char temp = str[start];
+                str[start] = str[end];
+                str[end] = temp;
+                start++;
+                end--;
+            }
+        } else {
+            i++; // Skip spaces
+        }
+    }
+
+    printf("Reversed words: %s\n", str);
+    return 0;
+}*/
